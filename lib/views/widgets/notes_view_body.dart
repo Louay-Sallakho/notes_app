ecf30bb0386f427b6962d24255cc83b453865f9c
Louay Sallakho <louay.1997.sla@gmail.com>
2023-04-18 16:40:25 +0300

    Created Floating Action Button

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'Notes_List_view.dart';
+import 'notes_list_view.dart';
 import 'custom_app_bar.dart';
-import 'custom_note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
