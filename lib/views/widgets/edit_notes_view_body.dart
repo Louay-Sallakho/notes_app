42eaeeb10c502cc94a2107a86d33e145956335ca
Louay Sallakho <louay.1997.sla@gmail.com>
2023-04-19 17:23:04 +0300

    Finished the EditNotes View

--- lib/views/widgets/edit_notes_view_body.dart
+++ lib/views/widgets/edit_notes_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
+import 'package:notes_app/views/widgets/custom_text_field.dart';
 
 class EditNotesViewBody extends StatelessWidget {
   const EditNotesViewBody({super.key});
@@ -9,7 +10,20 @@ class EditNotesViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Column(
-        children: const [CustomAppBar(title: "Edit Notes", icon: Icons.check)],
+        children: const [
+          CustomAppBar(title: "Edit Notes", icon: Icons.check),
+          SizedBox(
+            height: 28,
+          ),
+          CustomTextField(hint: "Title"),
+          SizedBox(
+            height: 28,
+          ),
+          CustomTextField(
+            hint: "Content",
+            maxLines: 5,
+          )
+        ],
       ),
     );
   }
