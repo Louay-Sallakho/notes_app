e09995c67387719e4219063ed72ffad791792ee7
Louay Sallakho <louay.1997.sla@gmail.com>
2023-04-19 03:04:25 +0300

    Added the button to the bottom sheet

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
 
+import 'custom_button.dart';
 import 'custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -9,22 +12,31 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16),
-      child: Column(
-        children: const [
-          SizedBox(
-            height: 20,
-          ),
-          CustomTextField(
-            hint: "Title",
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          CustomTextField(
-            hint: "content",
-            maxLines: 5,
-          )
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: const [
+            SizedBox(
+              height: 20,
+            ),
+            CustomTextField(
+              hint: "Title",
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            CustomTextField(
+              hint: "content",
+              maxLines: 5,
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            CustomButton(),
+            SizedBox(
+              height: 16,
+            )
+          ],
+        ),
       ),
     );
   }
