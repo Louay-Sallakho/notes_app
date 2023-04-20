a22b620846e827cd9e2240fd66c30fb85081defc
Louay Sallakho <louay.1997.sla@gmail.com>
2023-04-20 03:21:18 +0300

    Provided AddNoteCubit to the MaterialApp

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,8 +1,23 @@
-
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:notes_app/constants.dart';
+import 'package:notes_app/models/note_model.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+
+  addNote(NoteModel note) {
+    emit(AddNoteLoading());
+
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+
+      notesBox.add(note);
+      emit(AddNoteSuccess());
+    } catch (e) {
+      emit(AddNoteFailuer(errMessage: e.toString()));
+    }
+  }
 }
